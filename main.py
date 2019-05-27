67ebbcfe6a659747cebc6790c85cfd1aa0115a9a
WalJT <jttw259@zoho.com>
2019-05-27 13:16:06 +0100

    Added second window with useless buttons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is my first PyQt5 app..
 
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
 
 # Create Application
 app = QApplication([])
@@ -8,8 +8,16 @@ app = QApplication([])
 # Add Label
 label = QLabel("Hello World")
 
+# Layouts and Buttons
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+
 # Show?
 label.show()
+window.show()
 
 # Run app until user quits
 app.exec_()
