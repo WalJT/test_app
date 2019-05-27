831b2ddf6c4e7bcdbdc9c9f9a480950d41ebac53
WalJT <jttw259@zoho.com>
2019-05-27 14:03:13 +0100

    Added main code to main() function

--- main.py
+++ main.py
@@ -7,44 +7,48 @@ from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLay
 def close_when_clicked():
     app.Close()
 
-# Create Application
-app = QApplication([])
+def main():
+    # Create Application
+    app = QApplication([])
 
-# Use a Custom Style
-#app.setStyle("fusion")
+    # Use a Custom Style
+    #app.setStyle("fusion")
 
-# Using style sheet to modify button margins
-app.setStyleSheet("QPushButton { margin: 10px; }")
+    # Using style sheet to modify button margins
+    app.setStyleSheet("QPushButton { margin: 10px; }")
 
-# Add Label
-#label = QLabel("Hello World")
+    # Add Label
+    #label = QLabel("Hello World")
 
-# Modifications to Colour Pallet
-# palette = QPalette()
-# palette.setColor(QPalette.ButtonText, Qt.black)
-# palette.setColor(QPalette.Button, Qt.white)
-# palette.setColor(QPalette.Background, Qt.gray)
-# app.setPalette(palette)
+    # Modifications to Colour Pallet
+    # palette = QPalette()
+    # palette.setColor(QPalette.ButtonText, Qt.black)
+    # palette.setColor(QPalette.Button, Qt.white)
+    # palette.setColor(QPalette.Background, Qt.gray)
+    # app.setPalette(palette)
 
-# Layouts and Buttons
-window = QWidget()
-layout = QVBoxLayout()
+    # Layouts and Buttons
+    window = QWidget()
+    layout = QVBoxLayout()
 
-button1 = QPushButton('Usless Button')
-layout.addWidget(button1)
+    button1 = QPushButton('Usless Button')
+    layout.addWidget(button1)
 
-button2 = QPushButton('The Close Button')
-layout.addWidget(button2)
+    button2 = QPushButton('The Close Button')
+    layout.addWidget(button2)
 
-window.setLayout(layout)
-window.setWindowTitle("A Pointless Application")
+    window.setLayout(layout)
+    window.setWindowTitle("A Pointless Application")
 
-# Run a function when a button is clicked
-button2.clicked.connect(close_when_clicked)
+    # Run a function when a button is clicked
+    button2.clicked.connect(close_when_clicked)
 
-# Show?
-#label.show()
-window.show()
+    # Show?
+    #label.show()
+    window.show()
 
-# Run app until user quits
-app.exec_()
+    # Run app until user quits
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
